20502646aa5d321a62bfb7e1b3973aa3d80540d4
Dmitry556 <dmytropinchuk1@gmail.com>
2025-01-29 23:08:14 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import StaffingAnalyzer from '../components/StaffingAnalyzer';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-12">
+      <main className="container mx-auto px-4">
+        <StaffingAnalyzer />
+      </main>
+    </div>
+  );
+}
